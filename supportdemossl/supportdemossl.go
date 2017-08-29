4ccecc8e090c293383d430625ec162fc1c12479f
dlynch <dlynch@gopivotal.com>
2017-08-28 21:10:09 -0500

    complete ssl demo

--- supportdemossl/supportdemossl.go
+++ supportdemossl/supportdemossl.go
@@ -5,14 +5,14 @@ import(
   "net/http"
   "crypto/tls"
   "time"
-  //"osi-training/colors"
+  "crypto/x509"
+  "osi-training/colors"
+  "encoding/pem"
+  "strings"
 )
 
-var (
-  // HTTPServerPort listeng port for http server
-  HTTPServerPort = "8081"
-  
-  rootCA = `-----BEGIN CERTIFICATE-----
+var (  
+  rootCA = []byte(`-----BEGIN CERTIFICATE-----
 MIID9zCCAt+gAwIBAgIJAM2Us+7ONCpKMA0GCSqGSIb3DQEBCwUAMFoxCzAJBgNV
 BAYTAlVTMQswCQYDVQQIEwJJTDEQMA4GA1UEBxMHQ2hpY2FnbzEMMAoGA1UEChMD
 VVBSMQwwCgYDVQQLEwNFbmcxEDAOBgNVBAMTB3Vwci5sbGMwHhcNMTcwNTE2MDAz
@@ -35,87 +35,147 @@ nTUIbg6a7J3pKxGv47W73GdetsYhcjLnzEkuyK7FLPggQPS/vtRrMzqvEic6KGOJ
 1d2AX42dGaJO2iSY7P2Z4sQXUl4v5655qNgLeDe7i+BupSHX1HDIeendjRZc0DfJ
 0ptGQnuWMpu00Gk2JWVT25ehfjZ2dAJFt+6qi82VrzXZvLmkUm10OqdS4PIx52M5
 CqfuiOLUvw4prUk=
------END CERTIFICATE-----`
-
-  serverCert = []byte(`-----BEGIN CERTIFICATE-----
-MIIDNzCCAh8CCQC6+1HnfWbvijANBgkqhkiG9w0BAQsFADBaMQswCQYDVQQGEwJV
-UzELMAkGA1UECBMCSUwxEDAOBgNVBAcTB0NoaWNhZ28xDDAKBgNVBAoTA1VQUjEM
-MAoGA1UECxMDRW5nMRAwDgYDVQQDEwd1cHIubGxjMB4XDTE3MDUxNjAwNDAxNVoX
-DTE4MDkyODAwNDAxNVowYTELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAklMMRAwDgYD
-VQQHEwdDaGljYWdvMQwwCgYDVQQKEwNVUFIxDDAKBgNVBAsTA0VuZzEXMBUGA1UE
-AxMOZXJuZXN0LnVwci5sbGMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
-AQC0V2b+V3aH2MANQdECedtFQoy05JRwUrMcA0FUD21e2XOAfCmwEHWiyRidGRd1
-5T2jj7mXNKibmXB7WD5G+2NaniF2vFykex8xOoQBBu0WgXBld026vnAWhKi9H8qB
-H2XPJmO37Nk6zPVJZmxccjWwH/ORGEIEu+l9va9dVE7BXhwMo9FIm0xA48sOC15m
-OA84Wj3/g/FxHP+KoBlbMo5NxerR0u0P/t73v9CGQxiLObzc2pJamy2X4v4VyMLe
-FBIDOV8+n8GgzKIJjbZifbzFzBlVesdH9gVswm0rJt3wr0CD+DDfvIhLiIrw+EkG
-lkY9vPpyyJlh6Zwkqb/foUQDAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAIKtW4Mz
-6fqf3jDoc8KSELUzBjx+5kQYfIb7T0nWlu8dandtnaV0+yWzggQjSJvl9Cu81SaJ
-8GZZ+7aLhi+ooYfuzBj3B9lgkyvPO5MSqZihml/L7hdR8EVw/ChUBlr2ECMGJ45S
-e9eyCNtJvN2jnYGHy07kFc5k5leCWF2/bVD4HyX6dxHmjvf1YNpaoT2x3bLybwJt
-v9Ns2Ywl3Mexqt5+JgU0Wn9fSvFmtXPYUWmt+N30MQBb+dNQGK1m+PvceZpQUMJn
-QEIIjPsPreZ4+nshI7/vvx4P4INFsJRqFU2lQ0V7oZ3pqiOo2ZhqXptIMD7W4Pbl
-qk5nHsrG3ZQXcPw=
 -----END CERTIFICATE-----`)
 
-  serverKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAtFdm/ld2h9jADUHRAnnbRUKMtOSUcFKzHANBVA9tXtlzgHwp
-sBB1oskYnRkXdeU9o4+5lzSom5lwe1g+RvtjWp4hdrxcpHsfMTqEAQbtFoFwZXdN
-ur5wFoSovR/KgR9lzyZjt+zZOsz1SWZsXHI1sB/zkRhCBLvpfb2vXVROwV4cDKPR
-SJtMQOPLDgteZjgPOFo9/4PxcRz/iqAZWzKOTcXq0dLtD/7e97/QhkMYizm83NqS
-Wpstl+L+FcjC3hQSAzlfPp/BoMyiCY22Yn28xcwZVXrHR/YFbMJtKybd8K9Ag/gw
-37yIS4iK8PhJBpZGPbz6csiZYemcJKm/36FEAwIDAQABAoIBAFnM8/JOpy06F1gC
-oAs1lqREYUBqWigcZifazxsGm5WRflxKd4++gnVO4bzIk9AXGLxrgyTyCcuBemtR
-I6HA6FZlS5COdytIS939n2HLix+b0NYVtFT7I2FzKXsTa4kkk1O1cA5UGE+ZY8Lr
-B6Z5EJblMincBHPLBpegbsRwiM7sMYFjNFnbKxGCmNDmsffFC2Jk3iyJ7EBfSDdL
-h50rS6Qlsdh1/YfOHBoM15JdDtTucugYFToB0UAATjcSD22/9f4K9F5ezwHhVo7l
-qFnWzH/zII+xHRiRJL1l3sy2tmFxK5f5HYWJsBSzk8XqxW6ZZUkiZ0l81wSWdfxX
-R/G+MZECgYEA431U+mxFk55S48lN5YRG0FgJuJL1kHV3mk10J2jUEzqq01GcuM/e
-Vs8lPaMe8JSd7LNjl13ZlELyGhlBajRJZyT9qPkSmBg9moQ3VRQrlzIugX8M7MkD
-6igJHoiELFmWSHnb8F8lNsm21F21AruDB7+aMjVZGegG7TW38NDmPu0CgYEAyvFk
-GaQf1mBYP03oC7a5+rYTzLhhkHzJheIxNaPWcwvQybp7iNkZZN9X/fDNMomzPQJU
-B8Hx8W2HhT6X9Q3krp4mu3xcL4Fu4PyGuWuSeDynTPpsLyw7CQCEryaBuOJsuaaC
-kJ8o5q0z0FM7JxerxmdbjtWOfupxj1xJvotlQK8CgYEApLQER38GbNdPGh6QKGE5
-x/RFrX2xDxMNMglr1HCgQv/R2EeZWXEef+lgBB1K9FIVeN90do37Ts2dbWnlo5gR
-oPz2LlWOsdGzIGEjkpSU7tXlN9qdp/3tuKWd3J5oW5fNJ9IafBDW57Dpjx39ROov
-9vcxE7LuPMRx52JiN5d3bA0CgYAv6fgZOa+unIaZQ5qCkXytXamlDu8x/tRRgMrf
-gDQUa3i6+AVMlP0y3KxYry6zPOGNiOwv/LFTr+lsIxAbclFIjNxWLZFSQfvcsKJa
-SrSFIMTbHtDF/mpdwLqS48OC1CqZVl/DJ2Cvvbra95uiqisLJ8HtIiyHeHChSUe7
-7gtUzwKBgQDFI5FVx8LcBeieILa19DGTOZVVeLS3b5f9V6EMRa5Qu2NXS2DMro4w
-zy4zr1EP2V2posZnrMuuWT+Gr+B0vtDIKQkBW/dUwUTfbLqkJ+HV+yswIBUkecm8
-4KhyOAtNrhbCZSfERncgHiGPbSmogauD/VUu3mXEvVHrJTht7e41pg==
+serverBadHostCert = []byte(`-----BEGIN CERTIFICATE-----
+MIIDTjCCAjagAwIBAgIJALPcPP2kFjq3MA0GCSqGSIb3DQEBCwUAMFoxCzAJBgNV
+BAYTAlVTMQswCQYDVQQIEwJJTDEQMA4GA1UEBxMHQ2hpY2FnbzEMMAoGA1UEChMD
+VVBSMQwwCgYDVQQLEwNFbmcxEDAOBgNVBAMTB3Vwci5sbGMwHhcNMTcwODI5MDE0
+MzA0WhcNNDUwMTE0MDE0MzA0WjBOMQswCQYDVQQGEwJVUzELMAkGA1UECAwCSUwx
+EDAOBgNVBAcMB0NoaWNhZ28xDDAKBgNVBAoMA3VwcjESMBAGA1UEAwwJMTI3LjAu
+MC4yMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2Ie8KLSowaEdpVtd
+ENVKxHL05djwNe32y/+Z+lJwb5W0s6DK5earVUorgmqkhqEBmCRL7iRBG9tDKw0Z
+tdpV2PHADdwYX00eMQCuR90rBTzteGhqUtaniXf4zAwQ4lnic3tVzeRRTbpjjMds
+ml2nf0nQI++kuskb2uRNvcP/gi7gCatX1KJUjdeQVaeqG52c+ZDik+PDDRHmFCOm
+NXXTwUclF8P1Zm+Rh+1oBFUzrcJYmSzXbnUFYl2T45gU+uOwFPPF5lrlA59LpH5V
+eSrzOm7dYM5jD0i+iqQn9lXmiaUFCXxbhmFpo+d0WQFtzlklDBzoRbB11vAQdT/H
+gAVwQwIDAQABoyMwITAfBgNVHREEGDAWhwR/AAACgg5lcm5lc3QudXByLmxsYzAN
+BgkqhkiG9w0BAQsFAAOCAQEAIrVtPBs3ON0grQ9p0G8HN2Htxzp+XMb7PXqOPtTY
+4jKkIoMEO+o9ejJwCoCC8txXTcwmugJz4Qip+tf7jtL2J43b0dCgF9Xl0ObX6t2r
+tXoEfOUV1ELaZ8uzS/DMEvpWKTOdovmYRQTbIN3LQlota9nT7Z6/KqZq5QrvGuOI
+mdd2lRE1CRXQmlCO4+P3Xbq7oySDjCA8vwRPp1aZ0nbaNFXT/INwJhuXpP+C3WE7
+VL8fjOXoFAD1XtOi6gGDB+Ixe+dCc46foRoGwZ724UrcPdTLoHUlXCYQv4useuSO
+0AFkX4dnmDLyWHaEY+1R+xM0Cd39URCpTRj08HnyjRKyYg==
+-----END CERTIFICATE-----`)
+
+serverBadHostKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA2Ie8KLSowaEdpVtdENVKxHL05djwNe32y/+Z+lJwb5W0s6DK
+5earVUorgmqkhqEBmCRL7iRBG9tDKw0ZtdpV2PHADdwYX00eMQCuR90rBTzteGhq
+UtaniXf4zAwQ4lnic3tVzeRRTbpjjMdsml2nf0nQI++kuskb2uRNvcP/gi7gCatX
+1KJUjdeQVaeqG52c+ZDik+PDDRHmFCOmNXXTwUclF8P1Zm+Rh+1oBFUzrcJYmSzX
+bnUFYl2T45gU+uOwFPPF5lrlA59LpH5VeSrzOm7dYM5jD0i+iqQn9lXmiaUFCXxb
+hmFpo+d0WQFtzlklDBzoRbB11vAQdT/HgAVwQwIDAQABAoIBAQCTiLity5qIhCzc
+9EmPJKVTATjYO15AgKl+CeRyaSVvAkQjeFWHHAp1jJnkvKDpkl6xuVl7I3yxbE5x
+0PVJsUB5Fw9S4jpBBRyjKuGU2Z9sbD0po4t+cf+pbPM6pKYEdFYVdV3bccvr+CnI
+TEE1VMbBtht5wNku48R0+sX1nMxFeEUvwdw19fatt+fgPKWmt1847fDjVJHTBjN8
+V866hxzEjfO02EhMRw1PQNpNCIwxxnxZomBPF0umJ+E3XPXhm4AEv6h3vEkMgeDb
+4q4VxZeDPtxSpa8riAFbTO2/pLmncFDICk2VbMnO0fQoe4N4c5/8qCUIbz9VMKx9
+hXshqGrhAoGBAP31UhCnFuaW9HFC4VT8G2i4lJt1y4nAs2tlOkRAGYgsN5WMpZlM
+cwTtyVJKfX9KSCvhDwa5bTi1L1NSHpL3aXEehMr3dBfOt1nKhNrmOHxtu6r4fcBV
+MhESxg5QLMEEJloXFSLpqLzutg+ZW/Vtgts56H10WFIrXQjqhXy7rCppAoGBANpF
+Ye9cfyVXShlreORntClcBuV0cp1aOEKlZiMrRjdEO7bsou8RlfFizi9C4O18ssry
+znLH/1VahoJXWlA5qFpF5+GtXpH99LivA6klZFYd7CxysQ3EFIRVGel3RhczFtqY
+58SXBUtgLKQbXUINh97L4uJ+8s4BdESnfARNYXfLAoGBALY3QiW+rdjPsR5fHWyr
+40K1RbNxxpPVVycUn8T+tRMWnt9H5j3jM5fywYOw/PDLLJOHTQ/HFg4dOpKvFk7f
+BKHsgt1axcqbQMVFYSPrEdZ9qazX1Oqedmj3rnHwptvrDVWQwFKnkrHVGX6BzBCB
+/eUsXJs/UmvKv1s5YoQH6FUZAoGBALXHWsyxx+IdVETfUItIIq7fSY8G7/1ECd7X
+SLy5aXzVgrXRVdCsYKfrlbOeixy94VlL5cuJ7If1IFikzz5JBoBH/9nfPQdw7MJy
+XyYbUfi2at6JlhLU3hFnnnDlOKZRkhLzjPIYo/wmWIdDkuNTyXgvbQEcoLIYW/Bh
+W9NpbmcJAoGASpJSX8qDkmxbCIOldtIomdlzJATDByVILfrTGVOIUSh+FVHKtDgs
+yhWYP0vvpmxIKACgWhhSdJHfsrN8SQOkgX9LrI+wsTCm+RubbWa/6LmgOmwqUqjU
+aNVUmZp/6Jb9Izqa/Jo2Bisk0cE4Q/+msZqaU/h9jKcFHUhXuyU5sFU=
 -----END RSA PRIVATE KEY-----`)
+
+serverLocalHostCert = []byte(`-----BEGIN CERTIFICATE-----
+MIIDTjCCAjagAwIBAgIJALPcPP2kFjq2MA0GCSqGSIb3DQEBCwUAMFoxCzAJBgNV
+BAYTAlVTMQswCQYDVQQIEwJJTDEQMA4GA1UEBxMHQ2hpY2FnbzEMMAoGA1UEChMD
+VVBSMQwwCgYDVQQLEwNFbmcxEDAOBgNVBAMTB3Vwci5sbGMwHhcNMTcwODI5MDEz
+NTMzWhcNNDUwMTE0MDEzNTMzWjBOMQswCQYDVQQGEwJVUzELMAkGA1UECAwCSUwx
+EDAOBgNVBAcMB0NoaWNhZ28xDDAKBgNVBAoMA3VwcjESMBAGA1UEAwwJMTI3LjAu
+MC4xMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqb2NniDEYPRy2t95
+YUgO1GEyKsILhxcfPCcFYnXRiEV3clAIqVcXf3JiQA2Jm6vGPXijznbrTKUUtpaI
+/mfwFepUfGUwlYrVyjvE5T6L0c+nWfrRMI4u8XS2yUXtQkpaW35SfsgsrPkw7csx
+qxGaOCWRNL9tnulJFJAaDC/6gfQ9x42fZICD64dF/YdrR5MbrhSnMbYKKhiF1HcE
+pE8oDIm1mI5CiMOtANDyPv12SPSBy6AHcPcP9hpX3rCx5REl/0AVBtrvc0CJzDSP
+yUeR4BJWS/ETMBVWlvf8k2Ri3ZCwAG3Iv2nzYQWZKrlwbilVg3q+xmimDcHZVCHb
+28Y0qQIDAQABoyMwITAfBgNVHREEGDAWhwR/AAABgg5lcm5lc3QudXByLmxsYzAN
+BgkqhkiG9w0BAQsFAAOCAQEAHgtMyrlgE972wYS34r2BF1ODA/OUFZ3RdiJZ5MtF
+XAx4JNEKS9v0DBnSQSNFMevGdPnWwv2LAU33dxGbm2kT2bEkmquwJc8zyu9MgUxv
+Rw6q3i3rt9Wt2oCLSdZDN1iXgjHYCaHBWho6UNI+pwELnFdICLZLBOkHeB/vAI0K
+sL/gbRZLWbgn+jMR7JIaDgR0lSaTIac2PUvMnGeAB4WW1ptOz7HEC7Q5A8zrPOx7
+0h8etkwqIIA0oajUXok9A7RqNNOf2s3TF6993xShOdO+/hO/nJ0P1NAgZG4eYkUz
+76lMEh9XI8IDhV5Mx4gsyuAWB51Xsc3J/PORpB4335Qi1w==
+-----END CERTIFICATE-----`)
+
+serverLocalHostKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAqb2NniDEYPRy2t95YUgO1GEyKsILhxcfPCcFYnXRiEV3clAI
+qVcXf3JiQA2Jm6vGPXijznbrTKUUtpaI/mfwFepUfGUwlYrVyjvE5T6L0c+nWfrR
+MI4u8XS2yUXtQkpaW35SfsgsrPkw7csxqxGaOCWRNL9tnulJFJAaDC/6gfQ9x42f
+ZICD64dF/YdrR5MbrhSnMbYKKhiF1HcEpE8oDIm1mI5CiMOtANDyPv12SPSBy6AH
+cPcP9hpX3rCx5REl/0AVBtrvc0CJzDSPyUeR4BJWS/ETMBVWlvf8k2Ri3ZCwAG3I
+v2nzYQWZKrlwbilVg3q+xmimDcHZVCHb28Y0qQIDAQABAoIBAHWalqdLrqQ6WeWZ
+1kB3q4asfRSw80m61HJZ2to4JV/UOYkjOI0TgX1U5AvbFU+dOTHYBy6CqE6nRe2n
+6OzTWL3maHjzpzDFS5DdznLC3h8VT68BH7UTniS/J/HWGCfI2kfwAvpCeOmzkZoA
+Ib6T6JUkOjIgu7PRkEfF+7Yb++XTEar9W7l8MkWgB7nEClR/r5yYNS8RzqLSM9dx
+ay3XaXWeS/zW4IgKy9BpQt6kOQB9ID1tSCtx3bFVcgUFi++YwNe4yb2SptEdCRFf
+0uML3Q6XsLTWReSP8T+0emKk8JRNPmNv4qwu7l3JJbTn0fNTUTiv3xzVsy5SPxUa
+SiXHuIECgYEA0HuBUWandBPFd3q+6yJ2YnRRq13RsuqUrPVW5Ht8PGw/P7EiWBzU
+uo6yTGlUsxehtPZVppdJ+yo2L+UkQZGlgUSNx6LFCVXbjQ963cHl3V+gruJ4IMcS
+HXXZ8ofU+KexKfBLPuDRDLYiQR1USzBROvOvWa7NvnI09EIWg5JystECgYEA0G2J
+epaXGwHAhmAIU6AGHt6kmjHWFwGg9SAFmIbwi74D5hvQtq1tT1/DJhYGzfrGq/YL
+NTywS0erpUB7PlEgqxG5o+cHdUG4ZK5JjmGSkAFLPFKjmtIdfb6bn3XD3OxiV3B9
+nM6MZKGLDTy0kq8BqzxaZUkZ67qSxGh+ZWID6lkCgYBFQ1LyRUWKxerLyAbXvYpR
+KU3cvf2mEFM2pweoPvJGbLwSn/nGBkYSeMf5pODT4x0BLvnDr+2POTXpcZo7AnVW
+3fywf34wnMqlMahjNkD07AlJMMoyMZDuIrI25jO2LJgqU7/b8vrg2z3EHkdb94B1
+MnJmPDH0fKLlJ3OtYKEkkQKBgEmxsR5LCHpgEDZy1f7bYc6gYgqy/EN+K+7/t3rK
+m1qNgMtnolA02aVq8pEQ0K8bsAs1H5lfL+YuHR58whaykJ5r0fuFwDlRV2Uhypgx
+H6UTEArwHTCsggjn0BZ9iRcf7VWFTKSY00Lxazzu7dm/TxPAbyXIxwV2Hlabq7Ul
+BsVhAoGBALUhvg1Z41hxieK7Y0EN8Xzu6NtNRwR6N8q4a+l+C7gh+E/SQtj1onlp
+0zNuLyDvzjw9lzHKvA7J7gCJ2yyHBMmetD0pKC2ssp1d6ixmcUTS9nlKJl+Ku+GB
+8omqiy9IPQGNYZERg+9Mw8fC5LinH6FU6Ni6I1cFtKuC53MMLYO9
+-----END RSA PRIVATE KEY-----`)
+
 )
+
+func createPemCert(cert, key []byte) *x509.Certificate {
+  p, _ := pem.Decode(cert)
+  if p == nil {
+    panic(fmt.Sprintf("Could not parse pem cert"))
+  }
+  c, err := x509.ParseCertificate(p.Bytes)
+  if err != nil {
+    panic(fmt.Sprintf("Could not parse cert from decoded pem: %s", err))
+  }
+  return c
+}
+
+func createCert(cert, key []byte) tls.Certificate {
+  c, err := tls.X509KeyPair(cert, key)
+  if err != nil {
+    panic(fmt.Sprintf("Could not load certs: %s", err))
+  }
+  return c
+}
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("<html>hello</html>"))
 }
 
-func runHTTPServer(port string){
-  http.HandleFunc("/", handleRoot) 
+func runHTTPServer(cert, key []byte, port string){ 
 
-  
   certs := make([]tls.Certificate,1)
-  cert, err := tls.X509KeyPair(serverCert, serverKey)
-  if err != nil {
-    panic(fmt.Sprintf("Could not load certs: %s", err))
-  }
-  certs[0] = cert
+  certs[0] = createCert(cert, key)
   
   cfg := &tls.Config{
-        //MinVersion:               tls.VersionTLS12,
-        //CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
         PreferServerCipherSuites: true,
         CipherSuites: []uint16{
             tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
             tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
             tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
             tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-            tls.TLS_RSA_WITH_AES_256_CBC_SHA,
         },
-        Certificates:  certs,      
+        Certificates:  certs,     
   }
-  
   
   srv := &http.Server{
             Addr:         ":"+port,
@@ -123,22 +183,105 @@ func runHTTPServer(port string){
             //TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),            
   }
   
-  err = srv.ListenAndServeTLS("", "")
+  err := srv.ListenAndServeTLS("", "")
   if err != nil {
-     panic(fmt.Sprintf("Could not listen on port %s: %s", HTTPServerPort, err))
+     panic(fmt.Sprintf("Could not listen on port %s: %s", port, err))
   }
 }
 
 // Run performs the ssl tests
 func Run() {
-  go runHTTPServer("8081")
+  http.HandleFunc("/", handleRoot)
+  go runHTTPServer(serverBadHostCert, serverBadHostKey, "8081")
   time.Sleep(1 * time.Second)
-  // host verification failure 
+  
+  // host verification failure #################################################
+  fmt.Printf("Performing Host verfication failure test sending GET request https://127.0.0.1:8081: ")
   client := &http.Client{}
   _, err := client.Get("https://127.0.0.1:8081")
   if err != nil {
-    fmt.Println(err)
+    fmt.Printf("%s\n", colors.ColorizeError(err))
   }
-  // ca verification failure  
-  // cipher verification failure 
+  cert := createPemCert(serverBadHostCert, serverBadHostKey)
+  fmt.Printf(colors.ColorizeText(fmt.Sprintf(`
+Using cert:
+Subjects Common Name = %[1]s
+Issuer               = %[2]s
+
+Alt Names 
+%[3]s
+
+In this case the Subject "%[1]s" does not match 127.0.0.1 used in the get request 
+
+`, cert.Subject.CommonName, cert.Issuer.CommonName, strings.Join(cert.DNSNames, "\n") )))
+
+
+  // ca verification failure ###################################################
+  go runHTTPServer(serverLocalHostCert, serverLocalHostKey, "8082") 
+  time.Sleep(1 * time.Second)
+  fmt.Printf("Performing Certiicate Authority verfication failure test sending GET request https://127.0.0.1:8082: ")
+  _, err = client.Get("https://127.0.0.1:8082")
+  if err != nil {
+    fmt.Printf("%s\n", colors.ColorizeError(err))
+  }
+  cert = createPemCert(serverLocalHostCert, serverLocalHostKey)
+  fmt.Printf(colors.ColorizeText(fmt.Sprintf(`
+Using cert:
+Subjects Common Name = %[1]s
+Issuer               = %[2]s
+
+Alt Names 
+%[3]s
+
+In this case the Issue (Root Certificate Authority) "%[2]s" is not known to the client therefore can not be trusted 
+
+`, cert.Subject.CommonName, cert.Issuer.CommonName, strings.Join(cert.DNSNames, "\n") )))
+
+
+  // cipher verification failure ###############################################
+  go runHTTPServer(serverLocalHostCert, serverLocalHostKey, "8083") 
+  time.Sleep(1 * time.Second)
+  
+  // add the root certificate to the http client transport struct so we can verify the servers certificate
+  roots := x509.NewCertPool()
+  ok := roots.AppendCertsFromPEM(rootCA)
+  if !ok {
+    panic("Could not parse root cert")
+  }
+  
+  // restrict the cipher to something that is not supported by the server and create the new http client
+  cfg := &tls.Config{
+        PreferServerCipherSuites: true,
+        CipherSuites: []uint16{
+            tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+        }, 
+        RootCAs: roots,   
+  }
+  tr := &http.Transport{TLSClientConfig: cfg}
+  client = &http.Client{Transport: tr}
+  
+  fmt.Printf("Performing Cipher verfication failure test sending GET request https://127.0.0.1:8083: ")
+  _, err = client.Get("https://127.0.0.1:8083")
+  if err != nil {
+    fmt.Printf("%s\n", colors.ColorizeError(err))
+  }
+  cert = createPemCert(serverLocalHostCert, serverLocalHostKey)
+  fmt.Printf(colors.ColorizeText(fmt.Sprintf(`
+Using cert:
+Subjects Common Name = %[1]s
+Issuer               = %[2]s
+
+Alt Names 
+%[3]s
+
+Supported Client Cipher 
+TLS_RSA_WITH_AES_256_CBC_SHA
+
+Support Server Ciphers 
+TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+TLS_RSA_WITH_AES_256_GCM_SHA384,
+
+`, cert.Subject.CommonName, cert.Issuer.CommonName, strings.Join(cert.DNSNames, "\n") )))
 }
